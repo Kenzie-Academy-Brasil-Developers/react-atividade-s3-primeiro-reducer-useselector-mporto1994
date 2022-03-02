da34a9764a9686a4491f865823631c1dfe2bfdf1
Matheus <mporto1994@gmail.com>
2022-03-02 14:22:46 -0300

    feat: Add fruit

--- src/Store/modules/fruits/reducer.js
+++ src/Store/modules/fruits/reducer.js
@@ -2,6 +2,9 @@ const initialState = ["Banana", "Maçã", "Morango"];
 
 const fruitsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "ADD_FRUIT":
+        const { fruit } = action;
+        return [...state, fruit];
     default:
       return state;
   }
--- src/components/FruitsPage/index.jsx
+++ src/components/FruitsPage/index.jsx
@@ -1,12 +1,25 @@
 //importando useSelector do react-redux
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { addFruit } from "../../Store/modules/fruits/actions";
+
 
 const FruitsPage = () => {
+    const [fruit, setFruit] = useState("")
+    const dispatch = useDispatch();
 	//acessando o estado global e pegando o state fruits
-  const fruits = useSelector((state) => state.fruits);
+    const fruits = useSelector((state) => state.fruits);
+
+    const handleAddFruit = () => {
+        dispatch(addFruit(fruit))
+    }
 
   return (
     <div>
+        <input type="text" onChange={(e)=>setFruit(e.target.value)} />
+        <button onClick={()=>handleAddFruit}>Adicionar Fruta</button>
+
       {fruits.map((fruit) => (
         <p key={fruit}> {fruit} </p>
       ))}
