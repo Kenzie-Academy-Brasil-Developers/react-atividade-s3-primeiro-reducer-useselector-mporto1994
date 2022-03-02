c234de83a1b388ae13f104bb83cb2815ee99423f
Matheus <mporto1994@gmail.com>
2022-03-02 13:43:53 -0300

    feat: All done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FruitsPage from './components/FruitsPage/styles';
+
+function App() {
+  return (
+    <div className="App">
+      <FruitsPage/>
+    </div>
+  );
+}
+
+export default App;
